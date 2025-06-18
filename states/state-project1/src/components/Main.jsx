a61c21e205b8ec53914df8d8ddb049cba0094631
Faisal Javid <faisalljavid@gmail.com>
2025-06-19 00:49:08 +0530

    Add ingredient functionality to Main component with dynamic list rendering

--- states/state-project1/src/components/Main.jsx
+++ states/state-project1/src/components/Main.jsx
@@ -1,14 +1,30 @@
+import React, { useState } from "react"
+
 export default function Main() {
+    const [ingredients, setIngredients] = React.useState([])
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prev => [...prev, newIngredient])
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
