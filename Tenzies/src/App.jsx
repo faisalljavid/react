9306e32a4e2ba0d4bb352db15873cb685d51fbc1
Faisal Javid <faisalljavid@gmail.com>
2025-07-06 18:30:21 +0530

    Implement Tenzies game functionality with dice rolling feature; update App component to manage dice state, enhance styling in App.css, and add Die component for individual dice representation.

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react"
+import Die from './components/Die'
 import './App.css'
 
-
 export default function App() {
+
+    const [dice, setDice] = useState(generateAllNewDice())
+
+    function generateAllNewDice() {
+        const arry = []
+        for (let i = 0; i < 10; i++) {
+            let randomNum = Math.ceil(Math.random() * 6)
+            arry.push(randomNum)
+        }
+        return arry
+    }
+
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
+
+    const diceElements = dice.map(num => <Die value={num} />)
+
     return (
         <main>
-            Initial message
+            <div className="dice-container">
+                {diceElements}
+            </div>
+
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+
         </main>
     )
 }
