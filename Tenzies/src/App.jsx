dfb5d3b555d200e23dbcc32201efcce183d7d4a3
Faisal Javid <faisalljavid@gmail.com>
2025-07-07 16:09:43 +0530

    Update rollDice function in App component to reset dice when the game is won, enhancing game logic and user experience.

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -24,10 +24,15 @@ export default function App() {
     }
 
     function rollDice() {
-        setDice(oldDice => oldDice.map(die => {
-            return die.isHeld ? die :
-                { ...die, value: Math.ceil(Math.random() * 6) }
-        }))
+        if (!gameWon) {
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ? die :
+                    { ...die, value: Math.ceil(Math.random() * 6) }
+            }))
+        } else {
+            setDice(generateAllNewDice())
+        }
+
     }
 
     function hold(id) {
