c9570a7105e3b7022d32851fc608301096e3c96e
Faisal Javid <faisalljavid@gmail.com>
2025-07-07 15:56:11 +0530

    Integrate react-confetti for celebratory effects upon winning the Tenzies game; update App component to display confetti and modify button text based on game state. Adjust button styling in App.css for better responsiveness.

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
 import { nanoid } from "nanoid"
+import Confetti from "react-Confetti"
 import Die from './components/Die'
 import './App.css'
 
 export default function App() {
 
     const [dice, setDice] = useState(generateAllNewDice())
+
+    const gameWon = dice.every(die => die.isHeld) &&
+        dice.every(die => die.value === dice[0].value)
 
     function generateAllNewDice() {
         const arry = []
@@ -43,12 +47,15 @@ export default function App() {
 
     return (
         <main>
+            {gameWon ? <Confetti /> : null}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            <button className="roll-dice" onClick={rollDice}>
+                {gameWon ? "New Game" : "Roll"}
+            </button>
         </main>
     )
 }
