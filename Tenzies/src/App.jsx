c3934793f4b26182a851159ce701ff5523a7efc6
Faisal Javid <faisalljavid@gmail.com>
2025-07-07 14:17:52 +0530

    Update Tenzies game to use nanoid for unique dice identification; add nanoid dependency in package.json and package-lock.json, and enhance Die component styling based on dice state.

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { nanoid } from "nanoid"
 import Die from './components/Die'
 import './App.css'
 
@@ -9,8 +10,11 @@ export default function App() {
     function generateAllNewDice() {
         const arry = []
         for (let i = 0; i < 10; i++) {
-            let randomNum = Math.ceil(Math.random() * 6)
-            arry.push(randomNum)
+            arry.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: true,
+                id: nanoid()
+            })
         }
         return arry
     }
@@ -19,7 +23,9 @@ export default function App() {
         setDice(generateAllNewDice())
     }
 
-    const diceElements = dice.map(num => <Die value={num} />)
+    const diceElements = dice.map(dieObj => (
+        <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} />
+    ))
 
     return (
         <main>
