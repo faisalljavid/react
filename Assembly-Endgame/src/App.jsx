fb0e58c2e451bd0bdb3e121240d71e04fb460cf0
Faisal Javid <faisalljavid@gmail.com>
2025-07-09 16:23:34 +0530

    Add clsx dependency for conditional button styling in Assembly-Endgame. Update App.jsx to apply correct/wrong classes based on guessed letters. Enhance App.css with new styles for button states.

--- Assembly-Endgame/src/App.jsx
+++ Assembly-Endgame/src/App.jsx
@@ -4,11 +4,12 @@ import Header from "./components/Header"
 import Status from "./components/Status"
 import Eliminations from "./components/Eliminations"
 import { languages } from "./assets/languages"
+import { clsx } from "clsx"
 
 export default function AssemblyEndgame() {
     const [currentWord, setCurrentWord] = useState("react")
     const [guessedLetter, setGuessedLetter] = useState([])
-    
+
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
     function addGuessedLetter(letter) {
@@ -38,7 +39,20 @@ export default function AssemblyEndgame() {
     const keyboardElements = alphabet
         .split("")
         .map((letter) => {
-            return <button key={letter} onClick={() => addGuessedLetter(letter)}> {letter.toUpperCase()} </button>
+            const isGuessed = guessedLetter.includes(letter)
+            const isCorrect = isGuessed && currentWord.includes(letter)
+            const isWrong = isGuessed && !currentWord.includes(letter)
+            const className = clsx({
+                correct: isCorrect,
+                wrong: isWrong
+            })
+            console.log(className)
+            return <button
+                className={className}
+                key={letter}
+                onClick={() => addGuessedLetter(letter)}
+            >
+                {letter.toUpperCase()} </button>
         })
     return (
         <>
